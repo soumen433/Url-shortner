2fc3161b72a20bc611b1012119a32d9e1ca30f84
SHUBHAM VERMA <shubhamverma2604@gmail.com>
2021-12-14 16:33:25 +0530

    Update UrlModel.js

--- src/models/UrlModel.js
+++ src/models/UrlModel.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 var validUrl = require('valid-url');
 
+var expression = '^(?!mailto:)(?:(?:http|https|HTTP|HTTPS|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+var regex = new RegExp(expression);
+
 const UrlSchema = new mongoose.Schema({
 
     urlCode: { type: String, required: "urlCode is mandatory" , trim: true, unique: true,  lowercase: true},
@@ -19,7 +22,7 @@ const UrlSchema = new mongoose.Schema({
 
                     validator : function (longUrl) {
 
-                        if (!validUrl.isUri(longUrl)){
+                        if (!regex.test(longUrl)){
                             return 
                             
                         } 
